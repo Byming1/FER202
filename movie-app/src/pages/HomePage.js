c74cfe7a9a7b18365f4eb8455c790a982d6a2cf6
vcuongg <vc180605@gmail.com>
2025-12-08 11:23:26 +0700

    install axios

--- movie-app/src/pages/HomePage.js
+++ movie-app/src/pages/HomePage.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import { Container } from "react-bootstrap";
+import { Carousel, Container } from "react-bootstrap";
+import { instance } from "../axios/Axios";
 
 function HomePage() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    await instance.get("/movies").then((res) => {
+      setMovies(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <Container className="mt-5">
-        <h1>Welcome to CineMax</h1>
-        <p>Your favorite movies in one place</p>
+        <Carousel>
+          {movies.map((movie) => (
+            <Carousel.Item key={movie.id}>
+              <img
+                className="d-block w-100"
+                src={movie.img}
+                alt={movie.title}
+              />
+
+              <Carousel.Caption className="text-start">
+                <h3>{movie.title}</h3>
+                <p>{movie.desc}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
       </Container>
       <Footer />
     </div>
